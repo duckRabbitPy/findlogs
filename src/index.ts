692a3e6573966e89296a88878264b6db09ff2ce3
Oli Jones <78092825+duckRabbitPy@users.noreply.github.com>
2022-03-18 00:36:00 +0000

    Log if no console logs found

--- src/index.ts
+++ src/index.ts
@@ -7,32 +7,41 @@ const readFile = util.promisify(fs.readFile);
 
 //get all directory titles
 //loop through each directory and log if console.log() found
+//display no log found message if no logs found
 (async function findAndLog() {
   const allDirTitles = getDirRecursive(".").filter(
     (dirTitle) => !dirTitle.includes("node_modules")
   );
 
-  allDirTitles.forEach((dirTitle: string) => {
-    searchDir(dirTitle).catch(console.error);
+  const foundLogArr = Promise.all(
+    allDirTitles.map((dirTitle: string) => {
+      return searchDir(dirTitle).catch(console.error);
+    })
+  );
+
+  foundLogArr.then((arr) => {
+    if (arr.every((found) => found === false)) {
+      console.log("no logs found, directory is clean 🧼");
+    }
   });
 })();
 
 //open directory and loop through directory entries
 // pass current directory entry as argument to countAndDisplay()
+//return true if log found, false if no log found
 async function searchDir(dirTitle: string = "") {
-  let isClean = true;
   const dir = await fs.promises.opendir(`./${dirTitle}`);
   for await (const dirent of dir) {
     const filepath = process.cwd() + `/${dirTitle}/${dirent.name}`;
 
     if (filepath.slice(-3) === ".js" || filepath.slice(-3) === ".ts") {
-      const occurences = await countAndDisplay(filepath, dirent);
-      if (occurences > 0) {
-        isClean = false;
+      let occurrences = await countAndDisplay(filepath, dirent);
+      if (occurrences > 0) {
+        return true;
       }
     }
   }
-  return isClean;
+  return false;
 }
 
 //read directory entry contents and count occurrences of console.log()
